b0bba47a4f67ff1f6f9f02d4740f95b4eb71df6f
gromflomite <35708235+gromflomite@users.noreply.github.com>
2019-10-08 17:35:41 +0200

    Menus 1 and 2 worked.,3 work in progress.

--- Hotel.cs
+++ Hotel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace Hotel
 {
     internal class Hotel
     {
+        private static SqlConnection connection = new SqlConnection("Data Source=TRAVELMATE\\SQLEXPRESS;Initial Catalog=Hotels;Integrated Security=True");
+
         private static void Main(string[] args)
         {
             Menu();
@@ -19,6 +22,7 @@ namespace Hotel
             Console.WriteLine("\t 5. Check rooms");
             Console.WriteLine("\t 6. Quit application");
 
+            Console.Write("\n\t Enter option: ");
             int userSelect = int.Parse(Console.ReadLine());
 
             switch (userSelect)
@@ -50,40 +54,120 @@ namespace Hotel
 
         public static void clientSignUp()
         {
-            Console.Write("Enter the client name: ");
+            Console.WriteLine("\n - Client sign up - ");
+            Console.Write("\n\t Enter the client name: ");
             string clientName = Console.ReadLine();
 
-            Console.Write("Enter the client surname: ");
+            Console.Write("\n\t Enter the client surname: ");
             string clientSurname = Console.ReadLine();
 
-            Console.Write("Enter the client ID number: ");
+            Console.Write("\n\t Enter the client ID number: ");
             string clientID = Console.ReadLine();
 
-            string querySignUp = $"INSERT INTO Hotels VALUES('{clientName}','{clientSurname}','{clientID}'";
+            connection.Open();
 
+            string querySignup = $"INSERT INTO Clients (Firstname,Surname,DNI) values ('{clientName}','{clientSurname}','{clientID}')";
 
-
-
+            SqlCommand command = new SqlCommand(querySignup, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
         }
 
         public static void clientEdit()
         {
+            Console.WriteLine("\n - Client details editing - ");
+            Console.Write("\n\t Enter the client ID to check if exists: ");
+            string idCheck = Console.ReadLine();
 
+            string queryCheckId = $"SELECT * FROM Clients WHERE DNI = '{idCheck}'";
+
+            SqlCommand command = new SqlCommand(queryCheckId, connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                Console.Write("\n\t Enter the name of the client: ");
+                string clientName = Console.ReadLine();
+                Console.Write("\n\t Enter the surname of the client: ");
+                string clientSurname = Console.ReadLine();
+
+                string queryClientEdit = $"UPDATE Clients SET FirstName='{clientName}',Surname='{clientSurname}' WHERE dni='{idCheck}'";
+                SqlCommand commandIdUpdate = new SqlCommand(queryClientEdit, connection);
+                connection.Close();
+
+                connection.Open();
+                Console.WriteLine(commandIdUpdate.ExecuteNonQuery());
+                connection.Close();
+            }
+            else
+            {
+                Console.WriteLine("\n\t The client ID does not exists.");
+                connection.Close();
+                clientEdit();
+            }
+
+            connection.Close();
         }
 
         public static void clientCheckIn()
         {
+            Console.WriteLine("\n - Client check in - ");
+            Console.Write("\n\t Enter client ID: ");
+            string idCheck = Console.ReadLine();
 
+            string queryCheckId = $"SELECT * FROM Clients WHERE DNI = '{idCheck}'";
+
+            SqlCommand command = new SqlCommand(queryCheckId, connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                connection.Close();
+                string queryCheckRooms = $"SELECT ID FROM Rooms WHERE Available = 'Y'";
+                SqlCommand commandCheckRoom = new SqlCommand(queryCheckRooms, connection);
+
+                connection.Open();
+                SqlDataReader readerCheckRoom = commandCheckRoom.ExecuteReader();
+
+                Console.WriteLine("\n ID of available rooms: \n");
+                while (readerCheckRoom.Read())
+                {
+                    Console.WriteLine("\t" + readerCheckRoom[0].ToString());
+                }
+                connection.Close();
+
+                Console.Write("\n Select the ID of the available room: ");
+                string choosedRoom = Console.ReadLine();
+
+                string queryChoosedRoom = $"UPDATE Rooms SET Available = 'N' WHERE ID = '{choosedRoom}'";
+
+
+                SqlCommand commandChoosedRoom = new SqlCommand(queryChoosedRoom, connection);
+
+                connection.Open();
+                
+                Console.WriteLine("\n Your room has been reserved: " + commandChoosedRoom.ExecuteNonQuery() + " room modified.");
+            }
+            else
+            {
+                Console.WriteLine("\n\t The client ID does not exists. A user must be registered to be able to do a booking");
+                connection.Close();
+                clientSignUp();
+            }
         }
 
         public static void clientCheckOut()
         {
-
         }
 
         public static void roomCheck()
         {
-
         }
     }
 }
